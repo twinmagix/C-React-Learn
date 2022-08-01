7fe00a52a61ffcadca455f7b8f36c4492b41afc1
twinmagix <sazindwandwe1997@gmail.com>
2022-08-01 16:28:43 +0200

    sect five commit

---
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -0,0 +1,56 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
+
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/Activities').then(response => {
+    setActivities(response.data);
+    })
+  }, [])
+
+  function handleSelectActivity (id: string) {
+    setSelectedActivity(activities.find(x => x.id === id))
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
+  return (
+    <>
+       <NavBar openForm={handleFormOpen} />
+        <Container style={{marginTop: '7em'}}>
+          <ActivityDashboard 
+            activities={activities} 
+            selectedActivity={selectedActivity}
+            selectActivity={handleSelectActivity}
+            cancelSelectActivity={handleCancelSelectActivity}
+            editMode={editMode}
+            openForm={handleFormOpen}
+            closeForm={handleFormClose}
+          />
+        </Container>
+
+    </>
+  );
+}
+
+export default App;
