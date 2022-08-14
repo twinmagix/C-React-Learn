5145f123404ba7bc09973652e7acb8cd664e2589
twinmagix <sazindwandwe1997@gmail.com>
2022-08-14 22:04:16 +0200

    sec eight commit

--- Reactivities/client-app/src/app/stores/activityStore.ts
+++ Reactivities/client-app/src/app/stores/activityStore.ts
@@ -1,7 +1,6 @@
 import { makeAutoObservable, runInAction} from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
-import {v4 as uuid} from 'uuid';
 
 
 export default class ActivityStore {
@@ -38,13 +37,17 @@ export default class ActivityStore {
         let activity = this.getActivity(id);
         if (activity) {
             this.selectedActivity = activity;
+            return activity;
         } else {
             this.loadingInitial = true;
             try {
                     const activity = await agent.Activities.details(id);
                     this.setActivity(activity);
-                    this.selectedActivity = activity;
+                    runInAction(() => {
+                        this.selectedActivity = activity;
+                    }) 
                     this.setLoadingInitial(false);
+                    return activity;
             } catch (error) {
                 console.log(error);
                 this.setLoadingInitial(false);
@@ -68,7 +71,6 @@ export default class ActivityStore {
 
     createActivity = async (activity: Activity) => {
         this.loading = true;
-        activity.id = uuid();
         try{
             await agent.Activities.create(activity);
             runInAction(() => {
